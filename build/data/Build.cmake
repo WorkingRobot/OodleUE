fe9a2fdfc6df4c1819b63bd613e0eb2ad06d496d
Asriel Camora <asriel@camora.dev>
2023-01-18 15:09:02 -0800

    Fix build system

--- build/data/Build.cmake
+++ build/data/Build.cmake
@@ -1,5 +1,6 @@
 s_project(oodle::data::${PROJ_NAME} ${PROJ_TYPE})
-s_add_dir_recursive(${oodle_data_SOURCE_DIR})
+s_add_dir_recursive(${oodle_data_SOURCE_DIR}/base)
+s_add_dir_recursive(${oodle_data_SOURCE_DIR}/core)
 s_end_sources()
 
 s_include_directories(PRIVATE ${oodle_data_SOURCE_DIR}/core)
--- build/network/Build.cmake
+++ build/network/Build.cmake
@@ -1,7 +1,8 @@
 s_project(oodle::network::${PROJ_NAME} ${PROJ_TYPE})
-s_add_dir_recursive(${oodle_data_SOURCE_DIR})
-s_add_dir_recursive(${oodle_network_SOURCE_DIR}/network)
-s_del_file(${oodle_network_SOURCE_DIR}/network/testoo2netstub.cpp)
+s_add_dir_recursive(${oodle_data_SOURCE_DIR}/base)
+s_add_dir_recursive(${oodle_data_SOURCE_DIR}/core)
+s_add_dir_recursive(${oodle_data_SOURCE_DIR}/network)
+s_del_file(${oodle_data_SOURCE_DIR}/network/testoo2netstub.cpp)
 s_end_sources()
 
 s_include_directories(PRIVATE ${oodle_data_SOURCE_DIR}/core)
