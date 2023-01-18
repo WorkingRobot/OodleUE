84709bef5fb9968cf31064c062a3f06ecbf1c608
Asriel Camora <asriel@camora.dev>
2023-01-18 11:22:26 -0800

    Add CI building

---
+++ build/texture/include/oodle2texint.h
@@ -0,0 +1,3 @@
+#pragma once
+
+using OodleTex_RecordEntry = void(void* data, size_t size);
---
+++ build/texture/texturert/bc7prep_format.h
@@ -0,0 +1,33 @@
+#pragma once
+
+// Source: Engine\Plugins\Developer\TextureFormatOodle\Sdks\2.9.8\lib\Win64\oo2tex_win64_debug.lib
+// Used IDA to grab these values. I haven't tested the code, but I believe these are correct.
+
+#define BC7PREP_MODE0_SIZE 16
+#define BC7PREP_MODE1_SIZE 16
+#define BC7PREP_MODE2_SIZE 16
+#define BC7PREP_MODE3_SIZE 16
+#define BC7PREP_MODE4_SIZE 16
+#define BC7PREP_MODE5_SIZE 16
+#define BC7PREP_MODE6_SIZE 16
+#define BC7PREP_MODE7_SIZE 16
+#define BC7PREP_MODE8_SIZE 16
+#define BC7PREP_MODE9_SIZE 4
+
+#define BC7PREP_MODE0_SPLIT 8
+#define BC7PREP_MODE1_SPLIT 8
+#define BC7PREP_MODE2_SPLIT 12
+#define BC7PREP_MODE3_SPLIT 12
+#define BC7PREP_MODE4_SPLIT 6
+#define BC7PREP_MODE5_SPLIT 8
+#define BC7PREP_MODE6_SPLIT 8
+#define BC7PREP_MODE7_SPLIT 12
+#define BC7PREP_MODE8_SPLIT 0
+#define BC7PREP_MODE9_SPLIT 0
+
+#define BC7PREP_MODE_COUNT 10
+
+#define BC7PREP_FLAG_SPLIT0 1
+#define BC7PREP_MAXBLOCKS 0x8000000
+#define BC7PREP_HEADER_VERSION 1980
+#define BC7PREP_FLAG_SWITCH_COLORSPACE 0x10000
