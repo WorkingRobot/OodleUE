05cb4b13d6d00ee2ea8305879e2c6ba55450d12b
Asriel Camora <asriel@camora.dev>
2023-01-18 10:37:20 -0800

    Multithread downloads

--- download_oodle.py
+++ download_oodle.py
@@ -1,5 +1,8 @@
-import requests, gzip, os, shutil, glob, zipfile
+import gzip, os, shutil, glob, zipfile
 import xml.etree.ElementTree as ET
+
+from concurrent.futures import as_completed
+from requests_futures.sessions import FuturesSession
 
 print("Moving gitdeps", flush=True)
 os.replace("unreal/Engine/Build/Commit.gitdeps.xml", "Commit.gitdeps.xml")
@@ -30,25 +33,38 @@ for pack in manifest.find("Packs"):
             break
 
 print("::group::Grabbing files", flush=True)
-idx = 0
+session = FuturesSession(max_workers=32)
+futures = []
 for pack in packs:
-    idx += 1
-    pack_data = gzip.decompress(requests.get("%s/%s/%s" % (manifest.attrib["BaseUrl"], pack.attrib["RemotePath"], pack.attrib["Hash"])).content)
+    future = session.get("%s/%s/%s" % (manifest.attrib["BaseUrl"], pack.attrib["RemotePath"], pack.attrib["Hash"]))
+    future.pack_hash = pack.attrib["Hash"]
+    futures.append(future)
+
+idx = 0
+for future in as_completed(futures):
+    pack_data = gzip.decompress(future.result().content)
     for blob in blobs:
-        if blob.attrib["PackHash"] == pack.attrib["Hash"]:
-            file_name = None
-            for file in files:
-                if file.attrib["Hash"] == blob.attrib["Hash"]:
-                    file_name = file.attrib["Name"]
-                    break
-            if not file_name:
+        if blob.attrib["PackHash"] != future.pack_hash:
+            continue
+
+        file_name = None
+        for file in files:
+            if file.attrib["Hash"] != blob.attrib["Hash"]:
                 continue
-            size = int(blob.attrib["Size"])
-            offset = int(blob.attrib["PackOffset"])
-            os.makedirs(os.path.dirname(file_name), exist_ok=True)
-            with open(file_name, "wb") as f:
-                f.write(pack_data[offset:offset + size])
-            print("%s (%d/%d)" % (file_name, idx, len(packs)), flush=True)
+
+            file_name = file.attrib["Name"]
+            break
+        else:
+            continue
+
+        size = int(blob.attrib["Size"])
+        offset = int(blob.attrib["PackOffset"])
+        os.makedirs(os.path.dirname(file_name), exist_ok=True)
+        with open(file_name, "wb") as f:
+            f.write(pack_data[offset:offset + size])
+
+        idx += 1
+        print("%s (%d/%d)" % (file_name, idx, len(packs)), flush=True)
 print("::endgroup::", flush=True)
 
 print("Moving includes", flush=True)
@@ -57,7 +73,7 @@ shutil.copytree("unreal/Engine/Plugins/Developer/TextureFormatOodle/Sdks", "Engi
 shutil.copytree("unreal/Engine/Plugins/Compression/OodleNetwork/Sdks", "Engine/Plugins/Compression/OodleNetwork/Sdks", dirs_exist_ok=True)
 
 print("::group::Extracting zips", flush=True)
-for file in glob.glob("unreal/**/*.zip", recursive=True):
+for file in glob.glob("Engine/**/*.zip", recursive=True):
     print(file, flush=True)
     with zipfile.ZipFile(file, 'r') as zip_ref:
         zip_ref.extractall(os.path.dirname(file))
