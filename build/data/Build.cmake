698c449fbdb9896daea3cbf66edeb4e800b3b4a4
Asriel Camora <asriel@camora.dev>
2023-01-18 17:20:03 -0800

    Manually define build sources per dependency
    Cleans up exports to be identical to the prebuilt libs

--- build/data/Build.cmake
+++ build/data/Build.cmake
@@ -1,10 +1,11 @@
 s_project(oodle::data::${PROJ_NAME} ${PROJ_TYPE})
-s_add_dir_recursive(${oodle_data_SOURCE_DIR}/base)
-s_add_dir_recursive(${oodle_data_SOURCE_DIR}/core)
+s_add_dir(${oodle_data_SOURCE_DIR}/base)
+s_add_dir(${oodle_data_SOURCE_DIR}/core)
 s_end_sources()
 
-s_include_directories(PRIVATE ${oodle_data_SOURCE_DIR}/core)
-s_include_directories(PRIVATE ${oodle_data_SOURCE_DIR}/core/public)
+s_include_directories(PUBLIC ${oodle_data_SOURCE_DIR}/core)
+s_include_directories(PUBLIC ${oodle_data_SOURCE_DIR}/core/public)
 
 s_set_arch(AVX512)
-s_compile_definitions(PRIVATE ${PROJ_DEF} OODLE_BUILDING_DATA)
+s_compile_definitions(PUBLIC ${PROJ_DEF} OODLE_NNS)
+#s_compile_definitions(PRIVATE OODLE_BUILDING_DATA)
