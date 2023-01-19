03615bba0d7ed77e0faacab8424e64b8d9976461
Asriel Camora <asriel@camora.dev>
2023-01-18 17:35:31 -0800

    Fix debug build

--- download_oodle.py
+++ download_oodle.py
@@ -33,7 +33,7 @@ for pack in manifest.find("Packs"):
             break
 
 print("::group::Grabbing files", flush=True)
-session = FuturesSession(max_workers=32)
+session = FuturesSession()
 futures = []
 for pack in packs:
     future = session.get("%s/%s/%s" % (manifest.attrib["BaseUrl"], pack.attrib["RemotePath"], pack.attrib["Hash"]))
