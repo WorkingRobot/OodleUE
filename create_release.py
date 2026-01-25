9bd4cc1e644f2890785b970cc09b96ea488be4e6
Asriel Camora <asriel@camora.dev>
2026-01-25 15:44:45 -0800

    Moar zip

--- create_release.py
+++ create_release.py
@@ -3,19 +3,10 @@ import zipfile
 from glob import glob
 from pathlib import Path
 
-zip_files = glob("*/*.zip")
-grouped_zips = {}
-for name in zip_files:
-    parts = Path(name).stem.split('-')
-    name_type = '-'.join(parts[:-2] + parts[-1:])
-    if name_type not in grouped_zips:
-        grouped_zips[name_type] = [name]
-    else:
-        grouped_zips[name_type].append(name)
-
-for group, files in grouped_zips.items():
-    with zipfile.ZipFile(group + '.zip', mode='w', compression=zipfile.ZIP_DEFLATED) as zip_file:
-        for file in files:
-            with zipfile.ZipFile(file, mode='r') as sub_zip:
-                for n in sub_zip.namelist():
-                    zip_file.writestr(n, sub_zip.open(n).read())
+for file_path in glob("*/*.zip"):
+    asset_name = Path(file_path).stem
+    with zipfile.ZipFile(f"{asset_name}.zip", mode='w', compression=zipfile.ZIP_DEFLATED) as zip_file:
+        with zipfile.ZipFile(file_path, mode='r') as sub_zip:
+            for n in sub_zip.namelist():
+                new_name = n.replace("/Release/", "/").replace("/Debug/", "/")
+                zip_file.writestr(new_name, sub_zip.open(n).read())
